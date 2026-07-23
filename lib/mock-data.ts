25d4f2652cae72e481f3158f104c64f59d01850b
jhnlmryml <yjhonalmar@gmail.com>
2026-07-24 01:24:42 +0800

    first commit

---
+++ lib/mock-data.ts
@@ -0,0 +1,27 @@
+export const conversations = [
+    {
+        id: "1",
+        title: "Build Portfolio Website",
+        date: "Today",
+    },
+    {
+        id: "2",
+        title: "Next.js Landing Page",
+        date: "Today",
+    },
+    {
+        id: "3",
+        title: "React Interview Questions",
+        date: "Yesterday",
+    },
+    {
+        id: "4",
+        title: "Tailwind Components",
+        date: "Yesterday",
+    },
+    {
+        id: "5",
+        title: "Build SaaS Dashboard",
+        date: "Previous 7 Days",
+    },
+];
